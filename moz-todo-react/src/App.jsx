8f98bae3836a9828f0404b15f91c4a61360902b6
Gislene Sayuri <gskudo@yahoo.com.br>
2022-05-14 13:31:57 -0300

    componentes adicionados

--- moz-todo-react/src/App.jsx
+++ moz-todo-react/src/App.jsx
@@ -1,25 +1,15 @@
 import React from "react";
 import './index.css';
 
-import UserIcon from './assets/user.svg';
+import Header from "./components/Header";
+import Main from "./components/Main";
 
 function App() {
   return (
     <>
-    <header>
-      <div className="logo"></div>
-      <div className="menu">
-        <button className="btn first selected">Btn 01</button>
-        <button className="btn second">Btn 02</button>
-        <button className="btn third">Btn 03</button>
-        <button className="btn fourth">Btn 04</button>
-      </div>
-      <div className="user-profile">
-        <img src={UserIcon} alt="User Profile" />
-      </div>
-    </header>
-    <main></main>
-  </>
+      <Header />
+      <Main />
+    </>
   );
 }
 
---
+++ moz-todo-react/src/components/Header.jsx
@@ -0,0 +1,23 @@
+import React from "react";
+import UserIcon from "../assets/user.svg";
+import Button from "./Button";
+
+
+function Header() {
+  return (
+    <header>
+      <div className="logo"></div>
+      <div className="menu">
+          <Button className="selected" innerText="Home"/>
+          <Button innerText="Today"/>
+          <Button innerText="Weekly"/>
+          <Button innerText="Monthly"/>
+      </div>
+      <div className="user-profile">
+        <img src={UserIcon} alt="User Profile" />
+      </div>
+    </header>
+  );
+}
+
+export default Header;
