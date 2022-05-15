9053d1297ee1480c54c41885d3e2f1769f6d8b9d
Gislene Sayuri <gskudo@yahoo.com.br>
2022-05-14 21:01:53 -0300

    tranformei em forms com reset funcionando. falta o submit ]

--- moz-todo-react/src/components/Main.jsx
+++ moz-todo-react/src/components/Main.jsx
@@ -1,28 +1,55 @@
-import React from "react";
-import Input from "./Input";
-import Button from "./Button";
+import React, {useState} from "react";
 
 function Main() {
+  const [description , setDescription] = useState('');
+  const [date , setDate] = useState('');
+  const [local , setLocal] = useState('');
+
+  const handleChange = (e) => {
+    setDescription(e.target.value);
+  }
+  const handleDateChange = (e) => {
+    setDate(e.target.value);
+  }
+  const handleLocalChange = (e) => {
+    setLocal(e.target.value);
+  }
+
+  const handleSubmit= () => {
+    const description1 = description;
+    const date1 = date;
+    const local1 = local;
+    alert(description1 + date1 + local1)
+  }
+
+  const handleFormReset= (e) => {
+    setDescription("");
+    setDate("");
+    setLocal("");
+  }
+
   return (
     <main>
-        <section className="Add">
-            <div>
-                <Input className="inputDescription" type="text" placeholder="Descrição"></Input>
-                <Button className="btnadicionar" innerText="ADICIONAR"/>
-            </div>
-            <div>
-                <Input className="inputDate" type="date" placeholder="Data"></Input>
-                <Input className="inputLocal" type="text" placeholder="Local"></Input>
-                <Button className="btnlimpar" innerText="LIMPAR"/>
-            </div>
-
-        </section>
-        <section className="Show">  
-        
-        </section>
+      <section className="Add">
+        <div>
+            <form onSubmit={(e) => {handleSubmit(e)}} onReset={(e) => {handleFormReset(e)}}>
+              <div>
+                  <input type="text" value={description} className="inputDescription" placeholder="Descrição" required onChange={(e) => {handleChange(e)}} />
+                  <input type="submit" className="btnadicionar" value="ADICIONAR"/>
+              </div>
+              <div>
+                  <input type="date" value={date} className="inputDate" placeholder="Data" required onChange={(e) => {handleDateChange(e)}} />
+                  <input type="text" className="inputLocal" placeholder="Local" value={local} required onChange={(e) => {handleLocalChange(e)}} />
+                  <input type="reset" className="btnlimpar" value="LIMPAR" />
+              </div>
+             </form>
+         </div>
+      </section>
+      <section className="Show">         
+        <p></p>
+      </section>
     </main>
   )
 }
-
 
 export default Main;
